e2fcdb4c1b04767dece34a61293dc0f654be0610
jmetaj <92587070+jmetaj@users.noreply.github.com>
2023-05-25 13:00:59 +0300

    Add files via upload

--- Project Code/Subscription.java
+++ Project Code/Subscription.java
@@ -1,3 +1,34 @@
-public class Subscription {
-    private int subscription_id;
+class Subscription {
+    private String subscriptionId;
+    private int duration;
+    private double price;
+    
+    public void setSubscriptionId(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+    
+    public String getSubscriptionId() {
+        return subscriptionId;
+    }
+    
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+
+
+public static void main(String[] args) {
+// block of code 		
+}
 }
