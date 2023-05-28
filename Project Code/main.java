f39afb79c57a7a7e4de7e94d401e5291d41de2ce
DamiMikonos <dami.assimomitis@gmail.com>
2023-05-28 21:16:05 +0300

    Correct Main

---
+++ Project Code/main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class main {
+    public List<Gym> ListOfGyms = new ArrayList<Gym>();
+    /*Manage Hours of Gyms */
+    public void manageHours(){
+        /*Need to display List of Gyms Owned by called Owner */
+        for(int i=0; i<ListOfGyms.size(); i++){
+            if(ListOfGyms[1].getOwner() == Owner.getName()){}
+        }
+        /*Select and use gym instance to set Hours */
+    }
+    
+    /*Main program */
+    public static void main(String[] args){
+
+    }
+}
