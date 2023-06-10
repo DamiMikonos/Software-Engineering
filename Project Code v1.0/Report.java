00a5cec072add3428922dbba1a33fd94febf8a8e
jmetaj <92587070+jmetaj@users.noreply.github.com>
2023-06-10 16:57:04 +0300

    Update Report.java

--- Project Code v1.0/Report.java
+++ Project Code v1.0/Report.java
@@ -1,29 +1,46 @@
+import java.time.LocalDate;
+
 public class Report {
-    private int report_id;
+    private int reportId;
     private String status;
-    private String report_category;
-    private String report_description;
-    /*Date Class for report_date */
+    private String reportCategory;
+    private String reportDescription;
+    private LocalDate reportDate;
 
-    public Report(int report_id, String status, String report_category, String report_description) {
-        this.report_id = report_id;
+    public Report(int reportId, String status, String reportCategory, String reportDescription, LocalDate reportDate) {
+        this.reportId = reportId;
         this.status = status;
-        this.report_category = report_category;
-        this.report_description = report_description;
+        this.reportCategory = reportCategory;
+        this.reportDescription = reportDescription;
+        this.reportDate = reportDate;
     }
 
-    /*Get functions */
-    public int getID() {
-        return report_id;
+    public int getReportId() {
+        return reportId;
     }
+
     public String getStatus() {
         return status;
     }
+
     public String getReportCategory() {
-        return report_category;
-    }
-    public String getReportDescription() {
-        return report_description;
+        return reportCategory;
     }
 
+    public String getReportDescription() {
+        return reportDescription;
+    }
+
+    public LocalDate getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(LocalDate reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
